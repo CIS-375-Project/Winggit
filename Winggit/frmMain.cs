70bea35f43733661362044bcaddd6ed20757d44a
GaruJPN <ytaya@umich.edu>
2015-12-05 15:09:22 -0500

    Made frmSearch, added text and combo box

--- Winggit/frmMain.cs
+++ Winggit/frmMain.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Winggit
 {
@@ -15,6 +7,11 @@ namespace Winggit
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, System.EventArgs e)
+        {
+            new frmSearch().Show();
         }
     }
 }
