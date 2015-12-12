7d56166db637c057544ab4815c836da7702c1049
IJButler <ijbutler@umich.edu>
2015-12-12 12:29:54 -0500

    Removed function for running static query

--- Winggit/Controls/DBFunctions.cs
+++ Winggit/Controls/DBFunctions.cs
@@ -7,29 +7,6 @@ namespace Winggit.Controls
     internal static class DBFunctions
     {
         /// <summary>
-        /// Pass in a static SQL Query and get back a DataSet
-        /// </summary>
-        /// <param name="sql"></param>
-        /// <returns></returns>
-        internal static DataSet GetDataSet(string sql)
-        {
-            using (DataSet oDataSet = new DataSet())
-            {
-                using (SqlConnection oConnection = new SqlConnection(@"server=winggit.ddns.net\winggit_sql;database=winggit;uid=winggit_app;pwd=C@iro$88"))
-                {
-                    using (SqlCommand oCommand = new SqlCommand(sql, oConnection))
-                    {
-                        using (SqlDataAdapter oSqlDataAdaptor = new SqlDataAdapter(oCommand))
-                        {
-                            oSqlDataAdaptor.Fill(oDataSet);
-                        }
-                    }
-                }
-                return oDataSet;
-            }
-        }
-
-        /// <summary>
         /// Pass in parameterized SQL Query with Parameter List
         /// </summary>
         /// <param name="sql"></param>
@@ -59,6 +36,11 @@ namespace Winggit.Controls
             }
         }
 
+        /// <summary>
+        /// Run a query where no result is expected
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="oParmList"></param>
         internal static void RunQuery(string sql, Hashtable oParmList)
         {
             using (SqlConnection oConnection = new SqlConnection(@"server=winggit.ddns.net\winggit_sql;database=winggit;uid=winggit_app;pwd=C@iro$88"))
