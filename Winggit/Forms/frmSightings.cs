f8734d1ba95414256f909c4c32624c61321cfc63
David Stephanoff <dstephanoffjr@hotmail.com>
2015-12-13 17:48:55 -0500

    Fixed a bug where, when sighting, the system would not allow NULL to be a value for Country

--- Winggit/Forms/frmSightings.cs
+++ Winggit/Forms/frmSightings.cs
@@ -166,9 +166,9 @@ namespace Winggit.Forms
                     }
                     else
                     {
-                        oHash.Add("@City", null);
-                        oHash.Add("@State", null);
-                        oHash.Add("@Country", null);
+                        oHash.Add("@City", string.Empty);
+                        oHash.Add("@State", string.Empty);
+                        oHash.Add("@Country", string.Empty);
                     }
                     oHash.Add("@Temp", updTemperature.Value);
                     oHash.Add("@WingerNum", Winger.currentWinger.WingerNum);
@@ -176,9 +176,9 @@ namespace Winggit.Forms
                     {
                         oHash.Add("@Long", updLongitude.Value);
                         oHash.Add("@Lat", updLatitude.Value);
-            }
-            else
-            {
+                    }
+                    else
+                    {
                         oHash.Add("@Long", null);
                         oHash.Add("@Lat", null);
                     }                  
