b29479e79b81f45da1ebf7fe67da2606d93c10f7
programmer94 <mgutterman@sbcglobal.net>
2015-12-14 17:56:40 -0500

    File IO implementation additions.

--- Winggit/Controls/FileIO.cs
+++ Winggit/Controls/FileIO.cs
@@ -2,11 +2,14 @@
 using System.Data;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimpleFixedWidthParser;
 using Winggit.Entities;
 using static System.IO.Path;
 
@@ -23,14 +26,71 @@ namespace Winggit.Controls
                 Title = @"Please Select Your Sightings File"
             };
             if (ofd.ShowDialog() != DialogResult.OK) return;
-            FileStream sightingsStream = File.OpenRead(ofd.FileName);
-            ProcessFile(sightingsStream);
-        }
-
-        private void ProcessFile(FileStream fileStream)
-        {
-            
-            
+            DateTime testDate;
+            int testTag;
+            FixedWidthParser parser = new FixedWidthParser
+            (
+                ofd.FileName,
+                new List<dynamic>
+                {
+                    new FixedWidthColumn<DateTime>
+                    (
+                        "date",
+                        19,
+                        DateTime.Parse,
+                        date => date.ToString(@"yyyy-MM-dd"),
+                        test => DateTime.TryParse(test, out testDate)
+                    ),
+                    new FixedWidthColumn<string>
+                    (
+                        "latlong",
+                        22, // TODO might be 24
+                        latLongString => latLongString,
+                        latLong => latLong,
+                        test => true
+                    ),
+                    new FixedWidthColumn<string>
+                    (
+                        "city",
+                        29, // TODO might be 31
+                        cityString => cityString.Trim(),
+                        city => city,
+                        test => true
+                    ),
+                    new FixedWidthColumn<string>
+                    (
+                        "stateprov",
+                        29, // TODO might be 31
+                        stateProvString => stateProvString.Trim(),
+                        stateProv => stateProv,
+                        test => true
+                    ),
+                    new FixedWidthColumn<string>
+                    (
+                        "country",
+                        29, // TODO Verify length of country
+                        countryString => countryString.Trim(),
+                        country => country,
+                        test => true
+                    ),
+                    new FixedWidthColumn<string>
+                    (
+                        "species",
+                        20,
+                        speciesString => speciesString.Trim(),
+                        species => species,
+                        test => true
+                    ),
+                    new FixedWidthColumn<int>
+                    (
+                        "tag",
+                        11,
+                        int.Parse,
+                        tagString => tagString.ToString(),
+                        test => int.TryParse(test, out testTag)
+                    )
+                }
+            );
         }
 
         private void OutputFile()
