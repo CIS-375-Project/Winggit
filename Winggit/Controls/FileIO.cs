50135cdc9ea1da6a90fabf75d34af53e6b810f14
IJButler <ijbutler@umich.edu>
2015-12-15 02:44:48 -0500

    Finished file input

--- Winggit/Controls/FileIO.cs
+++ Winggit/Controls/FileIO.cs
@@ -21,6 +21,8 @@ namespace Winggit.Controls
        
         public static void FileInput()
         {
+            Hashtable oHash = new Hashtable();
+            string sql;
             OpenFileDialog ofd = new OpenFileDialog
             {
                 Filter = @"Text Files|*.txt",
@@ -28,49 +30,166 @@ namespace Winggit.Controls
             };
             if (ofd.ShowDialog() != DialogResult.OK) return;
             string[] file = File.ReadAllLines(ofd.FileName);
-            for (int i = 1; i < 6; i++)
+            int numRecords = int.Parse(file[file.Length - 1].Substring(3, 6).Trim());
+            if (numRecords == (file.Length - 2))
             {
-                List <string> parts = file[i].Split(' ').ToList();
-                parts.RemoveAll(isBlank);
-                if (parts.Count < 10)
+                for (int i = 1; i < (file.Length - 1); i++)
                 {
-                    MessageBox.Show(@"Not enough info given.", @"Insufficient info", MessageBoxButtons.OK);
-                    continue;
+                    int count = 0;
+                    oHash = new Hashtable();
+                    while (file[i].Length <= 172)
+                    {
+                        file[i] += " ";
+                    }
+                    string type = file[i].Substring(0, 1);
+                    string user = file[i].Substring(1, 11).Trim();
+                    string date = file[i].Substring(11, 20).Trim();
+                    string latlong = file[i].Substring(31, 23);
+                    string lat = latlong.Substring(0, 12);
+                    string log = latlong.Substring(12, 11);
+                    string city = file[i].Substring(54, 30).Trim();
+                    string state = file[i].Substring(85, 10).Trim();
+                    string country = file[i].Substring(110, 10).Trim();
+                    string species = file[i].Substring(145, 17).Trim();
+                    string tag = file[i].Substring(162, 9).Trim();
+                    if (type == "T")
+                    {
+                        Random rnd = new Random();
+                        int pTag = rnd.Next(999999999);
+                        oHash.Add("@Tracker", pTag);
+                        sql = "SELECT * FROM Butterflies WHERE Tracker_Num = @Tracker";
+                        DataSet check = DBFunctions.GetDataSet(sql, oHash);
+                        while (check.Tables.Count == 0 || check.Tables[0].Rows.Count == 0)
+                        {
+                            pTag = rnd.Next(999999999);
+                            oHash = new Hashtable();
+                            oHash.Add("@Tracker", pTag);
+                            sql = "SELECT * FROM Butterflies WHERE Tracker_Num = @Tracker";
+                            check = DBFunctions.GetDataSet(sql, oHash);
+                        }
+                        check.Dispose();
+                        oHash.Add(@"Species", species);
+                        sql = "INSERT INTO Butterflies OUTPUT.* VALUES(NULL,@Species,NULL,@Tracker";
+                        using (DataSet oDataSet = DBFunctions.GetDataSet(sql, oHash))
+                        {
+                            sql = "INSERT INTO Tags VALUES(";
+                            if (string.IsNullOrEmpty(date))
+                            {
+                                continue;
+                            }
+                            oHash.Add("@Date", DateTime.Parse(date));
+                            sql += "@Date,";
+                            if (string.IsNullOrEmpty(city))
+                            {
+                                sql += "NULL,";
+                            }
+                            else
+                            {
+                                oHash.Add("@City", city);
+                                sql += "@City,";
+                            }
+                            if (string.IsNullOrEmpty(state))
+                            {
+                                sql += "NULL,";
+                            }
+                            else
+                            {
+                                oHash.Add("@State", state);
+                                sql += "@State,";
+                            }
+                            if (string.IsNullOrEmpty(country))
+                            {
+                                sql += "NULL,";
+                            }
+                            else
+                            {
+                                oHash.Add("@Country", country);
+                                sql += "@Country,";
+                            }
+                            sql += "NULL,";
+                            if (string.IsNullOrEmpty(user))
+                            {
+                                continue;
+                            }
+                            oHash.Add("@UserID", int.Parse(user));
+                            sql += "@UserID,1,";
+                            decimal temp1 = decimal.Parse(lat);
+                            decimal temp2 = decimal.Parse(log);
+                            if (temp1 != 0 || temp2 != 0)
+                            {
+                                oHash.Add("@lat", temp1);
+                                oHash.Add("@long", temp2);
+                                sql += "@lat,@long,";
+                            }
+                            else
+                            {
+                                sql += "NULL,NULL,";
+                            }
+                            oHash.Add("@ID", oDataSet.Tables[0].Rows[0]["Tracker_Num"]);
+                            sql += "@ID)";
+                            DBFunctions.RunQuery(sql, oHash);
+                        }
+                    }
+                    else if (type == "S" && !string.IsNullOrEmpty(tag))
+                    {
+                        sql = "INSERT INTO Tags VALUES(";
+                        if (string.IsNullOrEmpty(date))
+                        {
+                            continue;
+                        }
+                        oHash.Add("@Date", DateTime.Parse(date));
+                        sql += "@Date,";
+                        if (string.IsNullOrEmpty(city))
+                        {
+                            sql += "NULL,";
+                        }
+                        else
+                        {
+                            oHash.Add("@City", city);
+                            sql += "@City,";
+                        }
+                        if (string.IsNullOrEmpty(state))
+                        {
+                            sql += "NULL,";
+                        }
+                        else
+                        {
+                            oHash.Add("@State", state);
+                            sql += "@State,";
+                        }
+                        if (string.IsNullOrEmpty(country))
+                        {
+                            sql += "NULL,";
+                        }
+                        else
+                        {
+                            oHash.Add("@Country", country);
+                            sql += "@Country,";
+                        }
+                        sql += "NULL,";
+                        if (string.IsNullOrEmpty(user))
+                        {
+                            continue;
+                        }
+                        oHash.Add("@UserID", int.Parse(user));
+                        sql += "@UserID,0,";
+                        decimal temp1 = decimal.Parse(lat);
+                        decimal temp2 = decimal.Parse(log);
+                        if (temp1 != 0 || temp2 != 0)
+                        {
+                            oHash.Add("@lat", temp1);
+                            oHash.Add("@long", temp2);
+                            sql += "@lat,@long,";
+                        }
+                        else
+                        {
+                            sql += "NULL,NULL,";
+                        }
+                        oHash.Add("@ID", tag);
+                        sql += "@ID)";
+                        DBFunctions.RunQuery(sql, oHash);
+                    }
                 }
-                if (parts[1].Any(char.IsLetter)) // IDs don't have letters.
-                {
-                    MessageBox.Show(@"ID is not a number.", @"Invalid ID", MessageBoxButtons.OK);
-                    continue;
-                }
-                DateTime testDate;
-                if (!DateTime.TryParse(parts[2] + " " + parts[3], out testDate))
-                {
-                    MessageBox.Show(@"Date cannot be parsed.", @"Invalid Date", MessageBoxButtons.OK);
-                    continue;
-                }
-                if (parts[4].Length != 22)
-                {
-                    MessageBox.Show(@"Latitude and Longitude not valid.", @"Invalid Lat/Long", MessageBoxButtons.OK);
-                    continue;
-                }
-                double lat = double.Parse(parts[4].Substring(1, 10));
-                lat *= parts[4][0] == '-' ? -1 : 1;
-                double longit = double.Parse(parts[4].Substring(12));
-                longit *= parts[4][11] == '-' ? -1 : 1;
-
-                if (parts[7] != "USA" && parts[7] != "Canada")
-                {
-                    MessageBox.Show(@"Not USA or Canada.", @"Invalid country", MessageBoxButtons.OK);
-                    continue;
-                }
-                List<string> statesProvs = parts[7] == "USA" ? Enum.GetNames(typeof (State)).ToList() : Enum.GetNames(typeof (Province)).ToList();
-                if (!statesProvs.Contains(parts[6]))
-                {
-                    MessageBox.Show(@"State/Province not valid.", @"Invalid state/province", MessageBoxButtons.OK);
-                    continue;
-                }
-                // TODO deal with multiword cities.
-                
             }
         }
 
