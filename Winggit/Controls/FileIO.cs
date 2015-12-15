26ccd9cd88746fda3ed7b6e0fc68a0f959cc1849
IJButler <ijbutler@umich.edu>
2015-12-14 23:04:56 -0500

    Restarted file IO

--- Winggit/Controls/FileIO.cs
+++ Winggit/Controls/FileIO.cs
@@ -27,89 +27,18 @@ namespace Winggit.Controls
                 Title = @"Please Select Your Sightings File"
             };
             if (ofd.ShowDialog() != DialogResult.OK) return;
-            DateTime testDate;
-            int testTag;
-            var parser = new FixedWidthParser
-            (
-                ofd.FileName,
-                new List<dynamic>
-                {
-                    new FixedWidthColumn<DateTime>
-                    (
-                        "date",
-                        19,
-                        DateTime.Parse,
-                        date => date.ToString(@"yyyy-MM-dd"),
-                        test => DateTime.TryParse(test, out testDate)
-                    ),
-                    new FixedWidthColumn<string>
-                    (
-                        "latlong",
-                        23,
-                        latLongString => latLongString,
-                        latLong => latLong.Trim(),
-                        test => true
-                    ),
-                    new FixedWidthColumn<string>
-                    (
-                        "city",
-                        29, // TODO might be 31
-                        cityString => cityString.Trim(),
-                        city => city,
-                        test => true
-                    ),
-                    new FixedWidthColumn<string>
-                    (
-                        "stateprov",
-                        29, // TODO might be 31
-                        stateProvString => stateProvString,
-                        stateProv => stateProv,
-                        test => true
-                    ),
-                    new FixedWidthColumn<string>
-                    (
-                        "country",
-                        29, // TODO Verify length of country
-                        countryString => countryString.Trim(),
-                        country => country,
-                        test => true
-                    ),
-                    new FixedWidthColumn<string>
-                    (
-                        "species",
-                        20,
-                        speciesString => speciesString.Trim(),
-                        species => species,
-                        test => true
-                    ),
-                    new FixedWidthColumn<int>
-                    (
-                        "tag",
-                        11,
-                        int.Parse,
-                        tagString => tagString.ToString(),
-                        test => int.TryParse(test, out testTag)
-                    )
-                }
-            );
-            parser.Read();
-            foreach (dynamic sighting in parser)
+            string[] file = File.ReadAllLines(ofd.FileName);
+            for (int i = 1; i < 6; i++)
             {
-                DateTime sightDate = sighting.date;
-                string latAndLong = sighting[1];
-                latAndLong = latAndLong.Trim();
-                double lat = double.Parse(latAndLong.Substring(0, 11), NumberStyles.AllowLeadingSign);
-                double longit = double.Parse(latAndLong.Substring(11), NumberStyles.AllowLeadingSign);
-                string tagID = sighting.tag.ToString();
-                string spec = sighting.species;
-                string ctry = sighting.country;
-                List<string> stateProvList = ctry == "USA" ? new List<string>(Enum.GetNames(typeof (State))) : new List<string>(Enum.GetNames(typeof (Province)));
-                int stateIndex = stateProvList.IndexOf(sighting.stateprov);
-                if (stateIndex > 0)
-                {
-                    // TODO Send to database.
-                }
+                List <string> parts = file[i].Split(' ').ToList();
+                parts.RemoveAll(isBlank);
+                string something = "other";
             }
+        }
+
+        private static bool isBlank(String s)
+        {
+            return s.ToLower().Equals("");
         }
 
         static void FileOutput()
